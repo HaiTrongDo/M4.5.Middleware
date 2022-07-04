aa4efa0a3dfcc39b85b153f616cdfe3a3afd9bb6
HaiTrongDo <dotronghai91@gmail.com>
2022-07-04 13:48:59 +0700

    ...

--- 5.5[Bai tap] Xay dung tinh nang kiem tra dang nhap truoc khi thuc hien request/dist/index.js
+++ 5.5[Bai tap] Xay dung tinh nang kiem tra dang nhap truoc khi thuc hien request/dist/index.js
@@ -8,11 +8,19 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const router_1 = __importDefault(require("./router/router"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const PORT = 3000;
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use((0, morgan_1.default)("common"));
 app.use((0, helmet_1.default)());
+const limiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+app.use(limiter);
 app.use(router_1.default);
 app.use((err, req, res, next) => {
     if (err) {
